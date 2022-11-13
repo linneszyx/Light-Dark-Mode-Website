ab7bf1b17a23f9110e6397ecd578b37789d696fe
Suryansh Upadhyay <itssuryanshcooldude346@gmail.com>
2022-11-13 19:30:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+function imageModes(color) {
+    image1.src = `img/${color}1.svg`;
+    image2.src = `img/${color}2.svg`;
+    image3.src = `img/${color}3.svg`;
+}
+function toggleMode(isLight){
+    nav.style.backgroundColor = isLight ? 'rgb(255 255 255 / 50%)' :'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = isLight ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)' ;
+    toggleIcon.children[0].textContent = isLight ? 'Light Mode' : 'Dark Mode';
+    isLight ? toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun') : toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon'); ;
+    isLight ? imageModes('d') : imageModes('l');
+}
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleMode(false);
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleMode(true);
+
+    }
+}
+toggleSwitch.addEventListener('change', switchTheme);
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleMode(false);
+    }
+}
